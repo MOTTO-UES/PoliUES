782f9e2462f5a29ffff230c006055a23c68ed6fd
kratos <bautista.insu@gmail.com>
2016-05-01 14:35:53 -0600

    metodo insertar reserva

--- app/src/main/java/poliues/eisi/fia/edu/sv/poliues/ControlBDPoliUES.java
+++ app/src/main/java/poliues/eisi/fia/edu/sv/poliues/ControlBDPoliUES.java
@@ -45,14 +45,58 @@ public class ControlBDPoliUES {
             super(context,BASE_DATOS,null,VERSION);
         }
 
+
         @Override
         public void onCreate(SQLiteDatabase db) {
+            try{
+                db.execSQL("create table reserva (idreserva  INTEGER NOT NULL PRIMARY KEY,idhorario INTEGER,facultad VARCHAR(25) NOT NULL,fechaingreso DATE NOT NULL,numeropersonas INTEGER NOT NULL, motivo VARCHAR(25) NOT NULL,descripcionreserva   VARCHAR(50) NOT NULL;");
+                db.execSQL("create table detallereserva ( iddetallereserva INTEGER NOT NULL PRIMARY KEY,idarea integer, idreserva INTEGER);");
 
+            }catch(SQLException e){
+
+                e.printStackTrace();
+            }
         }
-
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         }
+
+
     }
+    public void abrir() throws SQLException{
+        db = DBHelper.getWritableDatabase();
+        return;
+    }
+
+    public void cerrar(){
+        DBHelper.close();
+    }
+
+    public String insertar(Reserva reserva){
+
+        String regInsertados="Registro Insertado Nº= ";
+        long contador=0;
+
+        ContentValues reserva1 = new ContentValues();
+        reserva1.put("idreserva",reserva.getIdreserva());
+        reserva1.put("idhorario",reserva.getIdhorario());
+        reserva1.put("facultad", reserva.getFacultad());
+        reserva1.put("fechaingreso", reserva.getFechaingreso().toString());
+        reserva1.put("numeropersonas", reserva.getNumeropersonas());
+        reserva1.put("motivo", reserva.getMotivo());
+        reserva1.put("descripcionreserva ",reserva.getDescripcionreserva());
+        contador=db.insert("reserva", null, reserva1);
+
+        if(contador==-1 || contador==0)
+        {
+            regInsertados= "Error al Insertar el registro, Registro                           Duplicado. Verificar inserción";
+        }
+        else {
+            regInsertados=regInsertados+contador;
+        }
+        return regInsertados;
+
+    }
+
 }
