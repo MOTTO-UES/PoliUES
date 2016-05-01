b7279651b9c5bc23fb187416212174da2eb745ff
kratos <bautista.insu@gmail.com>
2016-05-01 16:25:52 -0600

    metodos 90 % de avance

--- app/src/main/java/poliues/eisi/fia/edu/sv/poliues/ControlBDPoliUES.java
+++ app/src/main/java/poliues/eisi/fia/edu/sv/poliues/ControlBDPoliUES.java
@@ -5,7 +5,10 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Build;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Rodrigo Daniel on 29/04/2016.
@@ -99,4 +102,130 @@ public class ControlBDPoliUES {
 
     }
 
+    public String actualizar(Reserva reserva){
+        if(verificarIntegridad(reserva, 6)){
+            String[] id = {String.valueOf(reserva.getIdreserva())};
+            ContentValues cv = new ContentValues();
+            cv.put("idhorario", reserva.getIdhorario());
+            cv.put("facultad", reserva.getFacultad());
+            cv.put("facultad", reserva.getFechaingreso().toString());
+            cv.put("facultad", reserva.getNumeropersonas());
+            cv.put("motivo", reserva.getMotivo());
+            cv.put("descripcionreserva ", reserva.getDescripcionreserva());
+            db.update("reserva", cv, "idreserva = ?", id);
+            return "Registro Actualizado Correctamente";
+        }else{
+            return "Registro con codigo " + reserva.getIdreserva() + " no existe";
+        }
+    }
+
+    public Reserva consultarReserva(String idreserva){
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        String[] id = {idreserva};
+        Cursor cursor = db.query("reserva", camposreserva, "idreserva = ?", id,
+                null, null, null);
+        if(cursor.moveToFirst()){
+            Reserva reserva = new Reserva();
+            reserva.setIdreserva(Integer.parseInt(cursor.getString(0)));
+            reserva.setIdhorario(Integer.parseInt(cursor.getString(1);
+            reserva.setFacultad(cursor.getString(2));
+            Date fechacreacion = null;
+            try {
+                fechacreacion = formato.parse(cursor.getString(3));
+            }
+            catch (ParseException ex)
+            {
+                System.out.println(ex);
+            }
+            reserva.setFechaingreso(fechacreacion);
+            reserva.setNumeropersonas(Integer.parseInt(cursor.getString(4));
+            reserva.setMotivo(cursor.getString(5));
+            reserva.setDescripcionreserva(cursor.getString(6));
+            return reserva;
+        }else{
+            return null;
+        }
+    }
+
+    public String eliminar(Reserva reserva){
+
+        String regAfectados="filas afectadas= ";
+        int contador=0;
+        String where="idreserva='"+reserva.getIdreserva()+"'";
+        where=where+" AND codmateria='"+reserva.getIdhorario()+"'";
+        where=where+" AND ciclo="+reserva.getFechaingreso();
+        contador+=db.delete("reserva", where, null);
+        regAfectados+=contador;
+        return regAfectados;
+    }
+
+
+
+    private boolean verificarIntegridad(Object dato, int relacion) throws SQLException{
+
+        switch(relacion){
+            case 1:
+            {
+                Reserva reserva = (Reserva)dato;
+                String[] id1 = {String.valueOf(reserva.getIdreserva())};
+                String[] id2 = {String.valueOf(reserva.getIdhorario())};
+                //abrir();
+                Cursor cursor1 = db.query("reserva", null, "idreserva = ?", id1, null,
+                        null, null);
+                Cursor cursor2 = db.query("detallereserva", null, "idhorario = ?", id2,
+                        null, null, null);
+                if(cursor1.moveToFirst() && cursor2.moveToFirst()){
+                    //Se encontraron datos
+                    return true;
+                }
+                return false;
+
+            }
+
+            case 2:
+            {
+
+            }
+
+            case 3:
+            {
+
+            }
+
+            case 4:
+            {
+
+            }
+
+            case 5:
+            {
+
+            }
+
+            case 6:
+            {
+                //verificar que exista Reserva
+                Reserva reservay = (Reserva)dato;
+                String[] idm = {String.valueOf(reservay.getIdreserva())};
+                abrir();
+                Cursor cm = db.query("reserva", null, "idreserva = ?", idm, null,
+                        null, null);
+                if(cm.moveToFirst()){
+                    //Se encontro la Reserva
+                    return true;
+                }
+                return false;
+            }
+
+            default:
+            {
+                return false;
+
+            }
+
+
+        }
+
+
+    }
 }
