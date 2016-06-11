00e48d43c6e5a4b4f4a761542d7a711ac2cb8a67
Rodrigo <rodrigoxj32@hotmail.com>
2016-06-11 14:50:14 -0600

    ingreso de clase conexion

---
+++ app/src/main/java/poliues/eisi/fia/edu/sv/poliues/ControladorServicio.java
@@ -0,0 +1,99 @@
+package poliues.eisi.fia.edu.sv.poliues;
+
+/**
+ * Created by Rodrigo Daniel on 11/06/2016.
+ */
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+
+public class ControladorServicio {
+
+    public static String obtenerRespuestaPeticion(String url, Context ctx) {
+        String respuesta = " ";
+
+        // Estableciendo tiempo de espera del servicio
+        HttpParams parametros = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(parametros, 3000);
+        HttpConnectionParams.setSoTimeout(parametros, 5000);
+
+
+        // Creando objetos de conexion
+        HttpClient cliente = new DefaultHttpClient(parametros);
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse httpRespuesta = cliente.execute(httpGet);
+            StatusLine estado = httpRespuesta.getStatusLine();
+            int codigoEstado = estado.getStatusCode();
+            if (codigoEstado == 200) {
+                HttpEntity entidad = httpRespuesta.getEntity();
+                respuesta = EntityUtils.toString(entidad);
+            }
+        } catch (Exception e) {
+            Toast.makeText(ctx, "Error en la conexion", Toast.LENGTH_LONG).show();
+            // Desplegando el error en el LogCat
+            Log.v("Error de Conexion", e.toString());
+        }
+        return respuesta;
+
+    }
+
+    public static String obtenerRespuestaPost(String url, JSONObject obj,
+                                              Context ctx) {
+        String respuesta = " ";
+        try {
+            HttpParams parametros = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(parametros, 3000);
+            HttpConnectionParams.setSoTimeout(parametros, 5000);
+
+            HttpClient cliente = new DefaultHttpClient(parametros);
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setHeader("content-type", "application/json");
+
+            StringEntity nuevaEntidad = new StringEntity(obj.toString());
+            httpPost.setEntity(nuevaEntidad);
+            Log.v("Peticion",url);
+            Log.v("POST", httpPost.toString());
+            HttpResponse httpRespuesta = cliente.execute(httpPost);
+            StatusLine estado = httpRespuesta.getStatusLine();
+
+            int codigoEstado = estado.getStatusCode();
+
+            if (codigoEstado == 200) {
+                respuesta = Integer.toString(codigoEstado);
+                Log.v("respuesta",respuesta);
+            }
+            else{
+                Log.v("respuesta",Integer.toString(codigoEstado));
+            }
+        } catch (Exception e) {
+            Toast.makeText(ctx, "Error en la conexion", Toast.LENGTH_LONG)
+                    .show();
+            // Desplegando el error en el LogCat
+            Log.v("Error de Conexion", e.toString());
+        }
+
+        return respuesta;
+    }
+
+
+
+
+
+}
