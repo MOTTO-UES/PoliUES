81ff8cfdba51cc682b401c8c3ca6c76c7b7f87e9
Dario Motto <dario_aratto@hotmail.com>
2016-06-12 13:46:50 -0600

    WS PHP MOTTO

---
+++ codigoPHP/ws_tarifa_insert.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+$CANTPERSONAS=$_REQUEST['cantPersonas'];
+$TARIFAUNITARIA=$_REQUEST['tarifaUnitaria'];
+$servidor="localhost";
+$usuario="root";
+$password="";
+$respuesta=array('resultado'=>0);
+json_encode($respuesta);
+$conexion=mysql_connect($servidor,$usuario,$password) or
+die ("Problemas en la conexion");
+$baseDatos="poliues";
+mysql_select_db($baseDatos,$conexion)
+  or  die("Problemas en la seleccion de la base de datos");
+$query = "INSERT INTO TARIFA (CANTPERSONAS,TARIFAUNITARIA)VALUES(".$CANTPERSONAS.",".$TARIFAUNITARIA.");";
+
+$resultadoQuery = mysql_query($query) or die(mysql_error());
+//Si la respuesta es correcta enviamos 1 y sino enviamos 0
+if(mysql_affected_rows() == 1)
+   $respuesta=array('resultado'=>1);
+echo json_encode($respuesta);
+mysql_close($conexion);
+?>
